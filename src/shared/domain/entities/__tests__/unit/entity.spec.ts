6bb47bf5f5483dcc0ac90df24e5789824722bca3
guilhermefparra <parra.guilherme2@gmail.com>
2026-03-01 12:12:34 -0300

    finish user module

---
+++ src/shared/domain/entities/__tests__/unit/entity.spec.ts
@@ -0,0 +1,43 @@
+import { Entity } from '../../entity';
+
+type StubProps = {
+  prop1: string;
+  prop2: number;
+};
+
+function uuidValidate(uuid: string): boolean {
+  const regex =
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/i;
+  return regex.test(uuid);
+}
+
+class StubEntity extends Entity<StubProps> {}
+
+describe('Entity unit tests', () => {
+  const props = { prop1: 'value1', prop2: 15 };
+  const id = '698b0c7e-c68b-4cfd-99dd-aac08024be8d';
+
+  it('Should set props and id', () => {
+    const entity = new StubEntity(props);
+
+    expect(entity.props).toStrictEqual(props);
+    expect(entity._id).not.toBeNull();
+    expect(uuidValidate(entity._id)).toBeTruthy();
+  });
+
+  it('Should accept a valid uuid', () => {
+    const entity = new StubEntity(props, id);
+
+    expect(uuidValidate(entity._id)).toBeTruthy();
+    expect(entity._id).toBe(id);
+  });
+
+  it('Should convert a entity to a Javascript Object', () => {
+    const entity = new StubEntity(props, id);
+
+    expect(entity.toJSON()).toStrictEqual({
+      id,
+      ...props,
+    });
+  });
+});
---
+++ src/users/domain/testing/helpers/user-data-builder.ts
@@ -0,0 +1,14 @@
+import {faker} from "@faker-js/faker"
+import { UserEntity, UserProps } from "../../entities/user.entity"
+
+type Props = Partial<UserProps>
+
+export function UserDataBuilder(props: Props): UserProps {
+
+  return {
+    name: props.name ?? faker.person.fullName(),
+    email: props.email ?? faker.internet.email(),
+    password: props.password ?? faker.internet.password(),
+    createdAt: props.createdAt ?? new Date()
+  }
+}
